2cbefe6e882ff2895d075d191a22d76058c3a36a
Gabriel Abdulai <53350925+ayichimboy@users.noreply.github.com>
2024-07-19 14:07:04 -0500

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,135 @@
-import streamlit as st
+#-------------------------------Import Libraries-----------------------------
 import pandas as pd
-import math
-from pathlib import Path
+import matplotlib.pyplot as plt
+import seaborn as sns
+import smartsheet
+import streamlit as st
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
+#CO5 WINTERSTEIGER NEW
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
+##------------------------OIL------------------------------------
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+API_TOKEN  = "4jPmJP29lZilsAf60FllSXAeX4A0uR29MXaSc"
+smartsheet_client = smartsheet.Smartsheet(API_TOKEN)
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
+c05_oil = "4062998129758084"
+sheet_oil = smartsheet_client.Sheets.get_sheet(c05_oil)
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+# Extract data and column names
+columns_oil = [col.title for col in sheet_oil.columns]
+data_oil = []
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+for row in sheet_oil.rows:
+    row_data = {}
+    for cell, col in zip(row.cells, columns_oil):
+        row_data[col] = cell.value
+    data_oil.append(row_data)
 
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
+# Convert to DataFrame
+df_oil = pd.DataFrame(data_oil)
 
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
-
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
+df_oil = df_oil.dropna(subset = ['Measured Oil'])
+df_oil["Date Created"] = pd.to_datetime(df_oil["Date Created"])
+df_oil = df_oil.sort_values("Date Created")
 
 
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
 
-st.header(f'GDP in {to_year}', divider='gray')
+##-------------------PROTEIN------------------------------
+c05_protein = "6368329476296580"
+sheet_protein = smartsheet_client.Sheets.get_sheet(c05_protein)
 
-''
+#print(sheet)
+# Extract data and column names
+columns_protein = [col.title for col in sheet_protein.columns]
+data_protein = []
 
-cols = st.columns(4)
+for row in sheet_protein.rows:
+    row_data = {}
+    for cell, col in zip(row.cells, columns_protein):
+        row_data[col] = cell.value
+    data_protein.append(row_data)
 
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
+# Convert to DataFrame
+df_protein = pd.DataFrame(data_protein)
 
-    with col:
-        first_gdp = first_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
+df_protein['Measured Protein'] = df_protein['Measured Protein'].replace(['N/A', 'NA'], pd.NA)
 
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
+# Filter out rows with NaN values in 'Measured Oil' column
+df_protein = df_protein.dropna(subset=['Measured Protein'])
+#df_protein = df_filtered[df_filtered['Date'].dt.year == 2024]
 
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+df_protein = df_protein[df_protein["Measured Protein"] > 35]
+
+df_protein["Date Created"] = pd.to_datetime(df_protein["Date Created"])
+df_protein = df_protein.sort_values("Date Created")
+
+#########------------------------------OIL----------------
+#print(df.head())
+# Streamlit app title
+st.title('Measured Oil Over Time')
+
+# Display the DataFrame
+st.write('### Data', df_oil)
+
+# Plot the data
+plt.figure(figsize=(10, 6))
+plt.plot(df_oil['Date Created'], df_oil['Measured Oil'], marker='o', linestyle='-', color='g')
+
+
+#mean
+plt.axhline(y=19.028, color='black', linestyle='--', linewidth=1)
+#2SD+
+plt.axhline(y=19.605739, color='orange', linestyle='-.', linewidth=1)
+#3SD+
+plt.axhline(y=19.894609, color='r', linestyle='-.', linewidth=1)
+#2SD-
+plt.axhline(y=18.450261, color='orange', linestyle='-.', linewidth=1)
+#3SD-
+plt.axhline(y=18.161391, color='r', linestyle='-.', linewidth=1)
+
+
+plt.title('Oil - C05 Wintersteiger [New]', fontsize = 16)
+plt.xlabel('Date Analyzed', fontsize = 16)
+plt.ylabel('Measured Oil', fontsize = 16)
+plt.grid(False)
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
+
+##------------------------------PROTEIN-----------------------
+
+#print(df.head())
+# Streamlit app title
+st.title('Measured Protein Over Time')
+
+# Display the DataFrame
+st.write('### Data', df_protein)
+
+# Plot the data
+plt.figure(figsize=(10, 6))
+plt.plot(df_protein['Date Created'], df_protein['Measured Protein'], marker='o', linestyle='-', color='g')
+
+
+#mean
+plt.axhline(y=44.74867, color='black', linestyle='--', linewidth=1)
+#2SD+
+plt.axhline(y=45.69376, color='orange', linestyle='-.', linewidth=1)
+#3SD+
+plt.axhline(y=46.1663, color='r', linestyle='-.', linewidth=1)
+#2SD-
+plt.axhline(y=43.80358, color='orange', linestyle='-.', linewidth=1)
+#3SD-
+plt.axhline(y=43.33103, color='r', linestyle='-.', linewidth=1)
+
+
+plt.title('Protein - C05 Wintersteiger [New]', fontsize = 16)
+plt.xlabel('Date Analyzed', fontsize = 16)
+plt.ylabel('Measured Protein', fontsize = 16)
+plt.grid(False)
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
+
+##-------------------------------END-----------------------------------------
